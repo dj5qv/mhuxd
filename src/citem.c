b58e6d2042ec294dee31c1bef170ebaa0966cac4
Matthias Moeller <mhuxd@maxec.de>
2013-11-08 23:57:51 +0100

    Implemented citem.c/.h, migrated mhmk2r.c to use new citem functions

---
+++ src/citem.c
@@ -0,0 +1,88 @@
+/*
+ *  mhuxd - mircoHam device mutliplexer/demultiplexer
+ *  Copyright (C) 2012  Matthias Moeller, DJ5QV
+ *
+ *  This program can be distributed under the terms of the GNU GPLv2.
+ *  See the file COPYING
+ */
+
+#include <string.h>
+#include "citem.h"
+#include "logger.h"
+
+const struct citem *citem_find(const struct citem *citems,  uint16_t num_citems, const char *key) {
+	uint16_t i;
+
+	for(i = 0; i < num_citems; i++) {
+		if(!strcmp(key, citems[i].key))
+			return &citems[i];
+	}
+	return NULL;
+}
+
+int citem_get_value(const struct citem *citems, int num_citems, const uint8_t *buffer, int buffer_size, const char *key) {
+
+	const struct citem *cp = citem_find(citems, num_citems, key);
+	if(!cp)
+		return -1;
+
+	uint16_t idx = cp->off + cp->base_bit / 8;
+	uint16_t bit = cp->base_bit % 8;
+	uint16_t mask = width2mask(cp->width);
+
+	if(idx >= buffer_size) {
+		err("(citem) %s() idx %d out of range for key '%s'!", __func__, idx, key);
+		return -1;
+	}
+
+	return (buffer[idx] >> (bit + 1 - cp->width)) & mask;
+}
+
+int citem_set_value(const struct citem *citems, int num_citems, uint8_t *buffer, int buffer_size, const char *key, int value) {
+	const struct citem *cp;
+	int c;
+	int idx, bit, mask;
+
+	cp = citem_find(citems, num_citems, key);
+	if(!cp) {
+		err("(citem) %s() unknown option: %s", __func__,  key);
+		return -1;
+	}
+
+	idx = cp->off + cp->base_bit / 8;
+	bit = cp->base_bit % 8;
+	mask = width2mask(cp->width);
+
+	if(idx >= buffer_size) {
+		err("(citem) %s() index out of range for %s", __func__, key);
+	}
+
+	c = buffer[idx];
+
+	if(value > mask) {
+		err("(citem) %s() invalid value %d for %s", __func__, value, key);
+		return -1;
+	}
+
+	c &= ~(mask << (bit + 1 - cp->width));
+	c |=  (value << (bit + 1 - cp->width));
+
+	buffer[idx] = c;
+
+	return 0;
+
+}
+
+void citem_debug_print_values(const char *header, struct citem *citems, int num_citems, const uint8_t *buffer, int buffer_size) {
+	uint16_t i;
+
+	if(log_get_level() < LOGSV_DBG1)
+		return;
+
+	for(i = 0; i < num_citems; i++) {
+		dbg1("(citem) %s %s: %d",
+		     header,
+		     citems[i].key,
+		     citem_get_value(citems, num_citems, buffer, buffer_size, citems[i].key));
+	}
+}
--- src/mhmk2r.c
+++ src/mhmk2r.c
@@ -10,192 +10,110 @@
 #include "mhmk2r.h"
 #include "util.h"
 #include "logger.h"
-
-struct citem {
-	const char	*key;
-	uint8_t		off;
-	uint8_t		base_bit;
-	uint8_t		width;
-};
+#include "citem.h"
 
 struct citem mok_items[] = {
-{ "txFocus", 0, 0, 1 },
-{ "rxFocus", 0, 1, 1 },
-{ "stereoFocus", 0, 2, 1 },
-{ "dvkFocus", 0, 3, 1 },
-{ "earsAuto", 0, 4, 1 },
-{ "directControl", 0, 5, 1 },
+	CITEM("txFocus", 0, 0, 1),
+	CITEM("rxFocus", 0, 1, 1 ),
+	CITEM("stereoFocus", 0, 2, 1 ),
+	CITEM("dvkFocus", 0, 3, 1 ),
+	CITEM("earsAuto", 0, 4, 1 ),
+	CITEM("directControl", 0, 5, 1 ),
 
-{ "lptCw", 0, 8, 1 },
-{ "lptCw", 0, 9, 1 },
-{ "lptTxFocusPin", 0, 10, 1 },
-{ "lptRxFocusPin", 0, 11, 1 },
-{ "lptStereoFocusPin", 0, 12, 1 },
-{ "focusAuto", 0, 13, 1 },
+	CITEM("lptCw", 0, 8, 1 ),
+	CITEM("lptCw", 0, 9, 1 ),
+	CITEM("lptTxFocusPin", 0, 10, 1 ),
+	CITEM("lptRxFocusPin", 0, 11, 1 ),
+	CITEM("lptStereoFocusPin", 0, 12, 1 ),
+	CITEM("focusAuto", 0, 13, 1 ),
 
-{ "ears.left.r1Main", 0, 16, 1 },
-{ "ears.left.r1Sub", 0, 17, 1 },
-{ "ears.left.scLeft", 0, 18, 1 },
-{ "ears.left.scRight", 0, 19, 1 },
-{ "ears.left.r2Main", 0, 20, 1 },
-{ "ears.left.r2Sub", 0, 21, 1 },
-{ "wkTxFocus", 0, 22, 1 },
-{ "wkKeyOutFocus", 0, 23, 1 },
+	CITEM("ears.left.r1Main", 0, 16, 1 ),
+	CITEM("ears.left.r1Sub", 0, 17, 1 ),
+	CITEM("ears.left.scLeft", 0, 18, 1 ),
+	CITEM("ears.left.scRight", 0, 19, 1 ),
+	CITEM("ears.left.r2Main", 0, 20, 1 ),
+	CITEM("ears.left.r2Sub", 0, 21, 1 ),
+	CITEM("wkTxFocus", 0, 22, 1 ),
+	CITEM("wkKeyOutFocus", 0, 23, 1 ),
 
-{ "ears.right.r1Main", 0, 24, 1 },
-{ "ears.right.r1Sub", 0, 25, 1 },
-{ "ears.right.scLeft", 0, 26, 1 },
-{ "ears.right.scRight", 0, 27, 1 },
-{ "ears.right.r2Main", 0, 28, 1 },
-{ "ears.right.r2Sub", 0, 29, 1 },
-{ "chanFocus", 0, 30, 1 },
-{ "lptFsIn", 0, 31, 1 },
+	CITEM("ears.right.r1Main", 0, 24, 1 ),
+	CITEM("ears.right.r1Sub", 0, 25, 1 ),
+	CITEM("ears.right.scLeft", 0, 26, 1 ),
+	CITEM("ears.right.scRight", 0, 27, 1 ),
+	CITEM("ears.right.r2Main", 0, 28, 1 ),
+	CITEM("ears.right.r2Sub", 0, 29, 1 ),
+	CITEM("chanFocus", 0, 30, 1 ),
+	CITEM("lptFsIn", 0, 31, 1 ),
 
-{ "a1SteppirVer", 4, 7, 8 },
-{ "a2SteppirVer", 5, 7, 8 },
-{ "a1SteppirVerHi", 6, 7, 8 },
-{ "a2SteppirVerHi", 7, 7, 8 },
+	CITEM("a1SteppirVer", 4, 7, 8 ),
+	CITEM("a2SteppirVer", 5, 7, 8 ),
+	CITEM("a1SteppirVerHi", 6, 7, 8 ),
+	CITEM("a2SteppirVerHi", 7, 7, 8 ),
 
 };
 
 struct citem mpk_items[] = {
-{ "lineUpstream", 0, 0, 1 },
-{ "micUpstream", 0, 1, 1 },
-{ "downstream", 0, 2, 1 },
-{ "audioCToAForced", 0, 3, 1 },
-{ "audioAToCForced", 0, 4, 1 },
-{ "frontMicSelected", 0, 5, 1 },
+	CITEM("lineUpstream", 0, 0, 1 ),
+	CITEM("micUpstream", 0, 1, 1 ),
+	CITEM("downstream", 0, 2, 1 ),
+	CITEM("audioCToAForced", 0, 3, 1 ),
+	CITEM("audioAToCForced", 0, 4, 1 ),
+	CITEM("frontMicSelected", 0, 5, 1 ),
 
-{ "sysPwrVoltage", 1, 7, 8 },
-{ "steppirVer", 2, 7, 8 },
-{ "steppirVerHi", 3, 7, 8 },
+	CITEM("sysPwrVoltage", 1, 7, 8 ),
+	CITEM("steppirVer", 2, 7, 8 ),
+	CITEM("steppirVerHi", 3, 7, 8 ),
 };
 
 struct citem mk2r_hfocus_items[] = {
-	{"txFocus", 0, 0, 1 },
-	{"rxFocus", 0, 1, 1 },
-	{"stereoFocus", 0, 2, 1 },
-	{"wkControledByApp", 0, 3, 1 },
-	{"directControl", 0, 4, 1 },
+	CITEM("txFocus", 0, 0, 1 ),
+	CITEM("rxFocus", 0, 1, 1 ),
+	CITEM("stereoFocus", 0, 2, 1 ),
+	CITEM("wkControledByApp", 0, 3, 1 ),
+	CITEM("directControl", 0, 4, 1 ),
 
-	{"ears.left.r1Main", 1, 0, 1 },
-	{"ears.left.r1Sub", 1, 1, 1 },
-	{"ears.left.scLeft", 1, 2, 1 },
-	{"ears.left.scRight", 1, 3, 1 },
-	{"ears.left.r2Main", 1, 4, 1 },
-	{"ears.left.r2Sub", 1, 5, 1 },
+	CITEM("ears.left.r1Main", 1, 0, 1 ),
+	CITEM("ears.left.r1Sub", 1, 1, 1 ),
+	CITEM("ears.left.scLeft", 1, 2, 1 ),
+	CITEM("ears.left.scRight", 1, 3, 1 ),
+	CITEM("ears.left.r2Main", 1, 4, 1 ),
+	CITEM("ears.left.r2Sub", 1, 5, 1 ),
 
-	{"ears.right.r1Main", 2, 0, 1 },
-	{"ears.right.r1Sub", 2, 1, 1 },
-	{"ears.right.scLeft", 2, 2, 1 },
-	{"ears.right.scRight", 2, 3, 1 },
-	{"ears.right.r2Main", 2, 4, 1 },
-	{"ears.right.r2Sub", 2, 5, 1 },
+	CITEM("ears.right.r1Main", 2, 0, 1 ),
+	CITEM("ears.right.r1Sub", 2, 1, 1 ),
+	CITEM("ears.right.scLeft", 2, 2, 1 ),
+	CITEM("ears.right.scRight", 2, 3, 1 ),
+	CITEM("ears.right.r2Main", 2, 4, 1 ),
+	CITEM("ears.right.r2Sub", 2, 5, 1 ),
 };
-
-static const struct citem *find_citem(struct citem *items,  uint16_t array_size, const char *key) {
-	uint16_t i;
-
-	for(i = 0; i < array_size; i++) {
-		if(!strcmp(key, items[i].key))
-			return &items[i];
-	}
-	return NULL;
-}
-
-static uint8_t width2mask(int w) {
-	return (0xff >> (8-w));
-}
 
 
 int mk2r_get_mok_value(const uint8_t mok_buffer[8], const char *key) {
-	const struct citem *cp = find_citem(mok_items, ARRAY_SIZE(mok_items), key);
-	if(!cp)
-		return -1;
 
-	uint16_t idx = cp->off + cp->base_bit / 8;
-	uint16_t bit = cp->base_bit % 8;
-	uint16_t mask = width2mask(cp->width);
-
-	if(idx >= 8) {
-		err("(mhstates) %s() idx %d out of range!", __func__, idx);
-		return -1;
-	}
-
-	return (mok_buffer[idx] >> (bit + 1 - cp->width)) & mask;
+	return citem_get_value(mok_items, ARRAY_SIZE(mok_items), mok_buffer, 8, key);
 }
 
 int mk2_get_mpk_value(const uint8_t mpk_buffer[4], const char *key) {
-	const struct citem *cp = find_citem(mpk_items, ARRAY_SIZE(mpk_items), key);
-	if(!cp)
-		return -1;
 
-	uint16_t idx = cp->off + cp->base_bit / 8;
-	uint16_t bit = cp->base_bit % 8;
-	uint16_t mask = width2mask(cp->width);
-
-	if(idx >= 4) {
-		err("(mhstates) %s() idx %d out of range!", __func__, idx);
-		return -1;
-	}
-
-	return (mpk_buffer[idx] >> (bit + 1 - cp->width)) & mask;
+	return citem_get_value(mpk_items, ARRAY_SIZE(mpk_items), mpk_buffer, 4, key);
 }
 
 
 void mk2r_debug_print_mok_values(const uint8_t mok_buffer[8]) {
-	uint16_t i;
 
-	if(log_get_level() < LOGSV_DBG1)
-		return;
-
-	for(i = 0; i < ARRAY_SIZE(mok_items); i++) {
-		dbg1("(mhstates) mok %s: %d", mok_items[i].key, mk2r_get_mok_value(mok_buffer, mok_items[i].key));
-	}
+	citem_debug_print_values("mok", mok_items, ARRAY_SIZE(mok_items), mok_buffer, 8);
 }
 
 void mk2_debug_print_mpk_values(const uint8_t mpk_buffer[4]) {
-	uint16_t i;
 
-	if(log_get_level() < LOGSV_DBG1)
-		return;
-
-	for(i = 0; i < ARRAY_SIZE(mpk_items); i++) {
-		dbg1("(mhstates) mpk %s: %d", mpk_items[i].key, mk2_get_mpk_value(mpk_buffer, mpk_items[i].key));
-	}
+	citem_debug_print_values("mpk", mpk_items, ARRAY_SIZE(mpk_items), mpk_buffer, 4);
 }
 
 
 int mk2r_set_hfocus_value(uint8_t hfocus_buffer[4], const char *key, int value) {
-	const struct citem *cp;
-	int c;
-	int idx, bit, mask;
+	dbg1("(mk2r) %s() %s: %d", __func__, key, value);
 
-	cp = find_citem(mk2r_hfocus_items, ARRAY_SIZE(mk2r_hfocus_items), key);
-	if(!cp) {
-		err("(mhstates) unknown host focus option: %s", key);
-		return -1;
-	}
-
-	idx = cp->off + cp->base_bit / 8;
-	bit = cp->base_bit % 8;
-	mask = width2mask(cp->width);
-
-	c = hfocus_buffer[idx];
-
-	if(value > mask) {
-		err("(mhstates) %s() invalid value %d for %s", __func__, value, key);
-		return -1;
-	}
-
-	c &= ~(mask << (bit + 1 - cp->width));
-	c |=  (value << (bit + 1 - cp->width));
-
-	hfocus_buffer[idx] = c;
-
-	dbg1("(mhstates) set host focus: %s = %d", key, value);
-
-	return 0;
+	return citem_set_value(mk2r_hfocus_items, ARRAY_SIZE(mk2r_hfocus_items), hfocus_buffer, 4, key, value);
 }
 
 static const char *copy_keys[] = {
