4d97da7a7b425f89cb865dd78bc38f27b5c53305
Matthias Moeller <mhuxd@maxec.de>
2017-05-07 01:38:16 +0200

    Added autoclean.sh, autogen.sh. Cleanup generated files.

---
+++ autoclean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -f aclocal.m4
+rm -f ar-lib
+rm -rf autom4te.cache
+rm -f compile
+rm -f configure
+rm -f depcomp
+rm -f install-sh
+rm -f Makefile.in
+rm -f missing
+rm -f config.h.in
