966c803b5aa51798191cc0c02ab84e2b7ffbad24
Matthias Moeller <mhuxd@maxec.de>
2017-05-07 22:35:35 +0200

    mhrouter.h cleanup

--- src/mhrouter.h
+++ src/mhrouter.h
@@ -23,22 +23,31 @@ typedef void (*MHRStatusCallback)(struct mh_router *, int status, void *user_dat
 typedef void (*MHRProcessorCallback)(struct mh_router *, int channel, struct buffer *, int fd, void *user_data);
 
 
+// core functions
 struct mh_router *mhr_create(struct ev_loop *loop, const char *serial, uint8_t has_flags_channel);
 void mhr_destroy(struct mh_router *router);
 void mhr_set_keyer_fd(struct mh_router *, int fd);
-void mhr_add_consumer(struct mh_router *router, int fd, int channel);
-void mhr_add_producer(struct mh_router *router, int fd, int channel);
-void mhr_add_consumer_cb(struct mh_router *router, MHRConsumerCallback, int channel, void *user_data);
-void mhr_add_status_cb(struct mh_router *router, MHRStatusCallback callback, void *user_data);
-void mhr_add_processor_cb(struct mh_router *router, MHRProcessorCallback callback, int channel, void *user_data);
-void mhr_rem_consumer(struct mh_router *router, int fd, int channel);
-void mhr_rem_producer(struct mh_router *router, int fd, int channel);
-void mhr_rem_consumer_cb(struct mh_router *router, MHRConsumerCallback, int channel);
-void mhr_rem_status_cb(struct mh_router *router, MHRStatusCallback callback);
-void mhr_rem_processor_cb(struct mh_router *router, MHRProcessorCallback callback, int channel);
+const char *mhr_get_serial(struct mh_router *);
 int mhr_send(struct mh_router *router, const uint8_t *data, unsigned int len, int channel);
 void mhr_set_bps_limit(struct mh_router *router, int channel, float bps);
 
-const char *mhr_get_serial(struct mh_router *);
+
+// consumer / producer interface
+void mhr_add_consumer(struct mh_router *router, int fd, int channel);
+void mhr_add_producer(struct mh_router *router, int fd, int channel);
+void mhr_add_consumer_cb(struct mh_router *router, MHRConsumerCallback, int channel, void *user_data);
+void mhr_rem_consumer(struct mh_router *router, int fd, int channel);
+void mhr_rem_producer(struct mh_router *router, int fd, int channel);
+void mhr_rem_consumer_cb(struct mh_router *router, MHRConsumerCallback, int channel);
+
+// processor interface (deprecated)
+void mhr_add_processor_cb(struct mh_router *router, MHRProcessorCallback callback, int channel, void *user_data);
+void mhr_rem_processor_cb(struct mh_router *router, MHRProcessorCallback callback, int channel);
+
+// generic callbacks
+void mhr_add_status_cb(struct mh_router *router, MHRStatusCallback callback, void *user_data);
+void mhr_rem_status_cb(struct mh_router *router, MHRStatusCallback callback);
+
+
 
 #endif // MHROUTER_H
