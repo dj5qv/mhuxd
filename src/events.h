361ebb4d375268f5c32592b2d53840a3e4b2469f
Matthias Moeller <dj5qv@maxec.de>
2026-09-21 22:55:08 +0200

    Remove unused eventbus events.

--- src/events.h
+++ src/events.h
@@ -16,30 +16,13 @@ struct mh_router;
 
 // Event Types
 enum app_event_type {
-    EV_DEV_HOTPLUG,       // OS-level connect/disconnect (udev)
     EV_KEYER_STATE,       // Keyer lifecycle (ONLINE, OFFLINE, etc.)
     EV_KEYER_MODE,        // Radio mode changed (CW, VOICE, etc.)
-    EV_KEYER_MOK_STATE,   // microKEYER internal state changed
-    EV_KEYER_ACC_STATE,   // Accessory state changed
     EV_CON_STATUS,        // Connector (TCP/VSP) status changed
     EV_MAX                // Keep as last
 };
 
 // Event Structs
-struct ev_dev_hotplug {
-    const char *serial;
-    int status;
-};
-
-struct ev_dev_lifecycle {
-    struct device *dev;
-};
-
-struct ev_router_status {
-    struct mh_router *router;
-    int status;
-};
-
 struct ev_keyer_state {
     const char *serial;
     int state;
@@ -50,12 +33,6 @@ struct ev_keyer_mode {
     uint8_t mode_cur;
     uint8_t mode_r1;
     uint8_t mode_r2;
-};
-
-struct ev_keyer_hw_state {
-    const char *serial;
-    const uint8_t *state;
-    uint8_t state_len;
 };
 
 struct ev_con_status {
